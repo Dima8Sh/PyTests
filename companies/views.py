253b04d165e1b15f3e219126055d2371b8eb2d9b
Dima8Sh <dirsherman8@gmail.com>
2022-02-14 01:37:06 +0200

    third

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .serializer import CompanySerializer
+
+
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer
+
